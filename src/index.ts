55f691550a110f0f4ac52657e941f0667976508d
Beetroot Paul <paul@beetrootpaul.com>
2023-07-22 10:23:40 +0200

    separate class/type/interface exports from const/fns exports

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,18 @@ export {
   CompositeColor as BpxCompositeColor,
   SolidColor as BpxSolidColor,
   TransparentColor as TransparentColor,
-  transparent_,
 } from "./Color";
 export { FillPattern as BpxFillPattern } from "./draw_api/FillPattern";
 export {
   type CharSprite as BpxCharSprite,
   type Font as BpxFont,
 } from "./font/Font";
-export { Sprite as BpxSprite, spr_ } from "./Sprite";
+export { Sprite as BpxSprite } from "./Sprite";
 export { Utils as BpxUtils } from "./Utils";
-export { Vector2d as BpxVector2d, v_ } from "./Vector2d";
+export { Vector2d as BpxVector2d } from "./Vector2d";
+
+export { transparent_ } from "./Color";
+export { spr_ } from "./Sprite";
+export { v_ } from "./Vector2d";
 
 export { BeetPx } from "./BeetPx";
--- ts_output/index.js
+++ ts_output/index.js
@@ -1,21 +1,24 @@
 "use strict";
 // noinspection JSUnusedGlobalSymbols
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BeetPx = exports.v_ = exports.BpxVector2d = exports.BpxUtils = exports.spr_ = exports.BpxSprite = exports.BpxFillPattern = exports.transparent_ = exports.TransparentColor = exports.BpxSolidColor = exports.BpxCompositeColor = void 0;
+exports.BeetPx = exports.v_ = exports.spr_ = exports.transparent_ = exports.BpxVector2d = exports.BpxUtils = exports.BpxSprite = exports.BpxFillPattern = exports.TransparentColor = exports.BpxSolidColor = exports.BpxCompositeColor = void 0;
 var Color_1 = require("./Color");
 Object.defineProperty(exports, "BpxCompositeColor", { enumerable: true, get: function () { return Color_1.CompositeColor; } });
 Object.defineProperty(exports, "BpxSolidColor", { enumerable: true, get: function () { return Color_1.SolidColor; } });
 Object.defineProperty(exports, "TransparentColor", { enumerable: true, get: function () { return Color_1.TransparentColor; } });
-Object.defineProperty(exports, "transparent_", { enumerable: true, get: function () { return Color_1.transparent_; } });
 var FillPattern_1 = require("./draw_api/FillPattern");
 Object.defineProperty(exports, "BpxFillPattern", { enumerable: true, get: function () { return FillPattern_1.FillPattern; } });
 var Sprite_1 = require("./Sprite");
 Object.defineProperty(exports, "BpxSprite", { enumerable: true, get: function () { return Sprite_1.Sprite; } });
-Object.defineProperty(exports, "spr_", { enumerable: true, get: function () { return Sprite_1.spr_; } });
 var Utils_1 = require("./Utils");
 Object.defineProperty(exports, "BpxUtils", { enumerable: true, get: function () { return Utils_1.Utils; } });
 var Vector2d_1 = require("./Vector2d");
 Object.defineProperty(exports, "BpxVector2d", { enumerable: true, get: function () { return Vector2d_1.Vector2d; } });
-Object.defineProperty(exports, "v_", { enumerable: true, get: function () { return Vector2d_1.v_; } });
+var Color_2 = require("./Color");
+Object.defineProperty(exports, "transparent_", { enumerable: true, get: function () { return Color_2.transparent_; } });
+var Sprite_2 = require("./Sprite");
+Object.defineProperty(exports, "spr_", { enumerable: true, get: function () { return Sprite_2.spr_; } });
+var Vector2d_2 = require("./Vector2d");
+Object.defineProperty(exports, "v_", { enumerable: true, get: function () { return Vector2d_2.v_; } });
 var BeetPx_1 = require("./BeetPx");
 Object.defineProperty(exports, "BeetPx", { enumerable: true, get: function () { return BeetPx_1.BeetPx; } });
