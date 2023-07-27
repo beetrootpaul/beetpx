5ded22fbf204c8e0ec80a67033c11215f34399be
Beetroot Paul <paul@beetrootpaul.com>
2023-07-28 01:04:52 +0200

    CharSprite#char

--- ts_output/font/Font.d.ts
+++ ts_output/font/Font.d.ts
@@ -3,6 +3,7 @@ import { Vector2d } from "../Vector2d";
 export type CharSprite = {
     positionInText: Vector2d;
     sprite: Sprite;
+    char: string;
 };
 export interface Font {
     spritesFor(text: string): CharSprite[];
