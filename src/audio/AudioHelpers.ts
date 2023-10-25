2e506d072ce8a0b6124de93e61ef27c39181ba9f
Beetroot Paul <paul@beetrootpaul.com>
2023-10-25 18:05:04 +0200

    fix mute/unmute in Chrome

--- src/audio/AudioHelpers.ts
+++ src/audio/AudioHelpers.ts
@@ -4,21 +4,24 @@ import { u_ } from "../Utils";
 export class AudioHelpers {
   static muteGain(
     gainNode: GainNode,
-    audioContextStartTime: number,
+    audioContextCurrentTime: number,
     fadeOutMillis: number,
     onMuted: () => void = u_.noop,
   ): void {
     fadeOutMillis = Math.max(0, fadeOutMillis);
+    Logger.debugBeetPx(
+      `AudioHelpers.muteGain (audioContextCurrentTime: ${audioContextCurrentTime}, fadeOutMillis: ${fadeOutMillis})`,
+    );
 
     AudioHelpers.#tryNTimes(15, 100, () => {
       if (fadeOutMillis > 0) {
         gainNode.gain.setValueCurveAtTime(
           [gainNode.gain.value, 0],
-          audioContextStartTime / 1000,
+          audioContextCurrentTime,
           fadeOutMillis / 1000,
         );
       } else {
-        gainNode.gain.setValueAtTime(0, audioContextStartTime);
+        gainNode.gain.setValueAtTime(0, audioContextCurrentTime);
       }
     }).catch(() => {});
 
@@ -29,21 +32,24 @@ export class AudioHelpers {
 
   static unmuteGain(
     gainNode: GainNode,
-    audioContextStartTime: number,
+    audioContextCurrentTime: number,
     fadeInMillis: number,
     onUnmuted: () => void = u_.noop,
   ): void {
     fadeInMillis = Math.max(0, fadeInMillis);
+    Logger.debugBeetPx(
+      `AudioHelpers.muteGain (audioContextCurrentTime: ${audioContextCurrentTime}, fadeInMillis: ${fadeInMillis})`,
+    );
 
     AudioHelpers.#tryNTimes(15, 100, () => {
       if (fadeInMillis > 0) {
         gainNode.gain.setValueCurveAtTime(
           [gainNode.gain.value, 1],
-          audioContextStartTime / 1000,
+          audioContextCurrentTime,
           fadeInMillis / 1000,
         );
       } else {
-        gainNode.gain.setValueAtTime(1, audioContextStartTime);
+        gainNode.gain.setValueAtTime(1, audioContextCurrentTime);
       }
     }).catch((err) => {});
 
