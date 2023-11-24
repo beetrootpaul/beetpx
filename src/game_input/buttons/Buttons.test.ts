a44dd45a53727c4b1a8a1501f1daacbb5dae0ded
Beetroot Paul <paul@beetrootpaul.com>
2023-11-24 19:03:35 +0100

    simplify vector equality checks in tests

--- src/game_input/buttons/Buttons.test.ts
+++ src/game_input/buttons/Buttons.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
+import { v_ } from "../../misc/Vector2d";
 import { Buttons } from "./Buttons";
 
 describe("Buttons", () => {
@@ -6,34 +7,34 @@ describe("Buttons", () => {
     const buttons = new Buttons();
 
     buttons.update(new Set(["button_left"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([-1, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(-1, 0));
     buttons.update(new Set(["button_right"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([1, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(1, 0));
     buttons.update(new Set(["button_up"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, -1]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, -1));
     buttons.update(new Set(["button_down"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 1]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 1));
 
     buttons.update(new Set([]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 0));
 
     buttons.update(new Set(["button_left", "button_right"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 0));
     buttons.update(new Set(["button_up", "button_down"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 0));
 
     buttons.update(new Set(["button_left", "button_up", "button_down"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([-1, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(-1, 0));
     buttons.update(new Set(["button_right", "button_up", "button_down"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([1, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(1, 0));
     buttons.update(new Set(["button_up", "button_left", "button_right"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, -1]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, -1));
     buttons.update(new Set(["button_down", "button_left", "button_right"]));
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 1]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 1));
 
     buttons.update(
       new Set(["button_left", "button_right", "button_up", "button_down"]),
     );
-    expect(buttons.areDirectionsPressedAsVector().asArray()).toEqual([0, 0]);
+    expect(buttons.areDirectionsPressedAsVector()).toEqual(v_(0, 0));
   });
 });
