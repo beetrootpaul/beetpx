c74e6be8427c1f1523054d1bde2d2264d25841e1
Beetroot Paul <paul@beetrootpaul.com>
2024-03-06 11:23:17 +0100

    fonts rework __wip__1

--- examples/fonts/src/CustomFont.ts
+++ examples/fonts/src/CustomFont.ts
@@ -1,99 +1,70 @@
-import {
-  BpxCharSprite,
-  BpxFont,
-  BpxFontId,
-  BpxImageUrl,
-  BpxPixels,
-  BpxRgbColor,
-  BpxVector2d,
-  v_,
-  v_0_0_,
-} from "../../../src";
+import { BpxFont, BpxPixels, spr_ } from "../../../src";
+import { BpxGlyph } from "../../../src/font/Font";
 
-// TODO: rework
-export class CustomFont implements BpxFont {
-  readonly id: BpxFontId = "custom-font";
-  readonly imageUrl: BpxImageUrl = "custom-font.png";
-  spriteTextColor = BpxRgbColor.fromCssHex("#99e550");
+export class CustomFont extends BpxFont {
+  ascent = 5;
+  descent = 3;
+  leading = 12;
 
-  #spriteFor(character: string): [BpxVector2d, BpxVector2d] | BpxPixels | null {
-    if (character === "T") {
-      return [v_(3, 0), v_(5, 8)];
+  spriteSheetUrls = ["custom-font.png"];
+
+  getGlyphFor(char: string): BpxGlyph {
+    switch (char) {
+      case "T":
+        return this.#spriteGlyph(5, 8, 3, 0);
+      case "b":
+        return this.#spriteGlyph(3, 8, 5, 5);
+      case "e":
+        return this.#spriteGlyph(3, 5, 8, 0);
+      case "f":
+        return this.#spriteGlyph(3, 8, 8, 5);
+      case "h":
+        return this.#spriteGlyph(3, 8, 0, 0);
+      case "n":
+        return this.#pixelsGlyph(`
+          ##-
+          #-#
+          #-#
+          #-#
+          #-#
+        `);
+      case "o":
+        return this.#spriteGlyph(3, 5, 5, 8);
+      case "r":
+        return this.#spriteGlyph(3, 5, 5, 0);
+      case "w":
+        return this.#pixelsGlyph(`
+          #-#-#
+          #-#-#
+          #-#-#
+          -#-#-
+          -#-#-
+        `);
+      case "x":
+        return this.#spriteGlyph(3, 5, 1, 8);
+      default:
+        return { type: "none" };
     }
-    if (character === "b") {
-      return [v_(5, 5), v_(3, 8)];
-    }
-    if (character === "e") {
-      return [v_(8, 0), v_(3, 5)];
-    }
-    if (character === "f") {
-      return [v_(8, 5), v_(3, 8)];
-    }
-    if (character === "h") {
-      return [v_(0, 0), v_(3, 8)];
-    }
-    if (character === "n") {
-      return BpxPixels.from(`
-        ##-
-        #-#
-        #-#
-        #-#
-        #-#
-      `);
-    }
-    if (character === "o") {
-      return [v_(5, 8), v_(3, 5)];
-    }
-    if (character === "r") {
-      return [v_(5, 0), v_(3, 5)];
-    }
-    if (character === "w") {
-      return BpxPixels.from(`
-        #-#-#
-        #-#-#
-        #-#-#
-        -#-#-
-        -#-#-
-      `);
-    }
-    if (character === "x") {
-      return [v_(1, 8), v_(3, 5)];
-    }
-    return null;
   }
 
-  // TODO: make it possible to define a font without a need to define this whole calculation process, just glyphs
-  spritesFor(text: string): BpxCharSprite[] {
-    const sprites: BpxCharSprite[] = [];
-    let positionInText = v_0_0_;
-    for (let i = 0; i < text.length; i += 1) {
-      const char = text[i]!;
-      // TODO: REWORK THIS
-      if (char === "\n") {
-        positionInText = v_(0, positionInText.y + 9);
-      } else {
-        const sprite = this.#spriteFor(char);
-        if (sprite) {
-          sprites.push({
-            char,
-            positionInText,
-            ...(sprite instanceof BpxPixels
-              ? { type: "pixels", pixels: sprite }
-              : { type: "image", spriteXyWh: sprite }),
-          });
-        }
-        positionInText = positionInText
-          .add(
-            sprite == null
-              ? 2
-              : sprite instanceof BpxPixels
-                ? sprite.wh.x
-                : sprite[1].x,
-            0,
-          )
-          .add(v_(1, 0));
-      }
-    }
-    return sprites;
+  #spriteGlyph(w: number, h: number, x: number, y: number): BpxGlyph {
+    // TODO: where to define the image URL?
+    const sprite = spr_("custom-font.png")(w, h, x, y);
+    return {
+      type: "sprite",
+      sprite: sprite,
+      advanceX: sprite.size.x + 1,
+    };
+  }
+
+  #pixelsGlyph(ascii: string): BpxGlyph {
+    const pixels = BpxPixels.from(ascii);
+    return {
+      type: "pixels",
+      pixels: pixels,
+      advanceX: pixels.size.x + 1,
+    };
   }
 }
+
+export const customFont = new CustomFont();
