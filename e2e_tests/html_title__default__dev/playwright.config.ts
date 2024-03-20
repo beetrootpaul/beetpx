ebd1eec73b34bf93998c634a5cb25c18b49361fc
Beetroot Paul <paul@beetrootpaul.com>
2024-03-20 18:49:13 +0100

    first real playwright test

--- e2e_tests/html_title__default__dev/playwright.config.ts
+++ e2e_tests/html_title__default__dev/playwright.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 // Docs: https://playwright.dev/docs/test-configuration
 export default defineConfig({
-  testDir: "tests",
-  fullyParallel: false,
-  forbidOnly: true,
-  retries: 0,
-  workers: 1,
+  testMatch: /.*\.e2e\.ts/,
+
+  timeout: 1000,
+
   reporter: "list",
+  quiet: false,
+  preserveOutput: "never",
+  fullyParallel: false,
+
+  forbidOnly: true,
+
+  workers: 1,
+
+  webServer: {
+    command: "npm install && npm start",
+    url: "http://localhost:5173",
+    timeout: 10_000,
+    reuseExistingServer: false,
+  },
+
   use: {
     baseURL: "http://localhost:5173",
     trace: "off",
-  },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-  ],
-  webServer: {
-    command:
-      "rimraf ./.beetpx/ && rimraf ./public/ && rimraf ./index.html && npm install && npm start",
-    url: "http://localhost:5173/index.html",
-    reuseExistingServer: false,
   },
 });
