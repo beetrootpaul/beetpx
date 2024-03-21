6cf9051d47df1bfebc1bda0f2214c7cc423b93f3
Beetroot Paul <paul@beetrootpaul.com>
2024-03-21 14:13:12 +0100

    wip

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+set -e
+
 npx playwright test --config e2e_tests/html_title__default__dev/playwright.config.ts
 npx playwright test --config e2e_tests/html_title__injected__dev/playwright.config.ts
-
