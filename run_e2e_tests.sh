aa101ef0f37c941ff7a4c4c984dc6a97af1ceaea
Beetroot Paul <paul@beetrootpaul.com>
2024-03-21 14:52:24 +0100

    todo

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+# TODO: run for all e2e files inside that dir instead of listing them one by one here
+
 npx playwright test --config e2e_tests/html_title__default__dev.config.ts
 npx playwright test --config e2e_tests/html_title__injected__dev.config.ts
