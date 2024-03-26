2a8095cede0aafb734e5a5d90bce6c0a5b6133f5
Beetroot Paul <paul@beetrootpaul.com>
2024-03-26 23:02:03 +0100

    a separate example for const injection (e.g. PREV_COMMIT)

--- examples/simple/src/beetpx.ts
+++ examples/simple/src/beetpx.ts
@@ -1,11 +1,5 @@
 import { b_, rgb_p8_, spr_, v_, v_0_0_ } from "../../../src";
 
-declare global {
-  interface Window {
-    PREV_COMMIT: string;
-  }
-}
-
 const logoSprite = spr_("logo.png")(16, 16, 0, 0);
 let circleMovementCenter = v_(64, 64);
 let logoPosition = v_0_0_;
@@ -38,10 +32,6 @@ b_.init({
   },
   onDraw() {
     b_.clearCanvas(rgb_p8_.storm);
-    b_.drawEllipseFilled(circleMovementCenter.sub(32 - 2), v_(64 - 4), rgb_p8_.moss);
-    b_.drawEllipse(circleMovementCenter.sub(32), v_(64), rgb_p8_.ember);
-    b_.drawEllipseOutsideFilled(circleMovementCenter.sub(32 + 2), v_(64 + 4), rgb_p8_.moss);
     b_.drawSprite(logoSprite, logoPosition, { centerXy: [true, true] });
-    b_.drawText(`PREV_COMMIT=${window.PREV_COMMIT}`, v_(1, 122), rgb_p8_.dusk);
   },
 });
