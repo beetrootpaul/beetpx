dca226d8d8e4307d70100e8a8b7fda2164439525
Beetroot Paul <paul@beetrootpaul.com>
2024-03-31 17:05:16 +0200

    add missing TimerSequence exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -464,6 +464,38 @@ declare class BpxTimer {
     restart(): void;
 }
 
+declare function timerSeq_<TPhaseName extends string>(params: {
+    intro?: Array<[phase: TPhaseName, frames: number]>;
+    loop?: Array<[phase: TPhaseName, frames: number]>;
+}, opts?: {
+    pause?: boolean;
+    delayFrames?: number;
+}): BpxTimerSequence<TPhaseName>;
+declare class BpxTimerSequence<TPhaseName extends string> {
+    #private;
+    static of<TPhaseName extends string>(params: {
+        intro: Array<[phase: TPhaseName, frames: number]>;
+        loop: Array<[phase: TPhaseName, frames: number]>;
+    }, opts: {
+        pause: boolean;
+        delayFrames: number;
+    }): BpxTimerSequence<TPhaseName>;
+    private constructor();
+    get justFinishedPhase(): TPhaseName | null;
+    get currentPhase(): TPhaseName | null;
+    get t(): number;
+    get progress(): number;
+    get framesLeft(): number;
+    get tOverall(): number;
+    get framesLeftOverall(): number;
+    get progressOverall(): number;
+    get hasFinishedOverall(): boolean;
+    get hasJustFinishedOverall(): boolean;
+    pause(): void;
+    resume(): void;
+    restart(): void;
+}
+
 declare class AudioApi {
     #private;
     constructor(assets: Assets, audioContext: AudioContext);
@@ -804,4 +836,4 @@ declare global {
     const BEETPX__VERSION: string;
 }
 
-export { BeetPx, BpxAnimatedSprite, type BpxAudioPlaybackId, type BpxBrowserType, BpxCanvasSnapshotColorMapping, type BpxCharSprite, type BpxColorMapper, BpxDrawingPattern, BpxEasing, type BpxEasingFn, type BpxFont, type BpxFontAsset, type BpxFontId, BpxFontSaint11Minimal4, BpxFontSaint11Minimal5, type BpxGameButtonName, type BpxGameInputEvent, type BpxGamepadType, BpxGamepadTypeDetector, type BpxImageAsset, type BpxImageUrl, type BpxJsonAsset, type BpxJsonUrl, BpxPatternColors, BpxPixels, BpxRgbColor, type BpxRgbCssHex, type BpxSoundAsset, type BpxSoundSequence, type BpxSoundSequenceEntry, type BpxSoundUrl, BpxSprite, BpxSpriteColorMapping, BpxTimer, BpxUtils, BpxVector2d, aspr_, b_, rgb_, rgb_black_, rgb_blue_, rgb_cyan_, rgb_green_, rgb_magenta_, rgb_p8_, rgb_red_, rgb_white_, rgb_yellow_, spr_, timer_, u_, v_, v_0_0_, v_1_1_ };
+export { BeetPx, BpxAnimatedSprite, type BpxAudioPlaybackId, type BpxBrowserType, BpxCanvasSnapshotColorMapping, type BpxCharSprite, type BpxColorMapper, BpxDrawingPattern, BpxEasing, type BpxEasingFn, type BpxFont, type BpxFontAsset, type BpxFontId, BpxFontSaint11Minimal4, BpxFontSaint11Minimal5, type BpxGameButtonName, type BpxGameInputEvent, type BpxGamepadType, BpxGamepadTypeDetector, type BpxImageAsset, type BpxImageUrl, type BpxJsonAsset, type BpxJsonUrl, BpxPatternColors, BpxPixels, BpxRgbColor, type BpxRgbCssHex, type BpxSoundAsset, type BpxSoundSequence, type BpxSoundSequenceEntry, type BpxSoundUrl, BpxSprite, BpxSpriteColorMapping, BpxTimer, BpxTimerSequence, BpxUtils, BpxVector2d, aspr_, b_, rgb_, rgb_black_, rgb_blue_, rgb_cyan_, rgb_green_, rgb_magenta_, rgb_p8_, rgb_red_, rgb_white_, rgb_yellow_, spr_, timerSeq_, timer_, u_, v_, v_0_0_, v_1_1_ };
