bec76444ffbf502e89bd6e5032282d41420c6e72
Beetroot Paul <paul@beetrootpaul.com>
2024-04-03 01:11:51 +0200

    added timer sequence in pause/restart example

--- examples/pause-and-restart/src/Vfx.ts
+++ examples/pause-and-restart/src/Vfx.ts
@@ -2,9 +2,9 @@ import {
   b_,
   BpxDrawingPattern,
   BpxEasing,
-  BpxTimer,
+  BpxTimerSequence,
   rgb_p8_,
-  timer_,
+  timerSeq_,
   v_,
 } from "../../../src";
 
@@ -16,11 +16,23 @@ export class Vfx {
     -#-#
   `);
 
-  constructor(params: { loopFrames: number }) {
-    this.#timer = timer_(params.loopFrames, { loop: true });
-  }
+  #loopFrames: number;
+  #timer: BpxTimerSequence<"a" | "b">;
 
-  #timer: BpxTimer;
+  constructor(params: { loopFrames: number }) {
+    // this.#timer = timer_(params.loopFrames, { loop: true });
+    this.#loopFrames = params.loopFrames;
+    this.#timer = timerSeq_({
+      intro: [
+        ["a", params.loopFrames - 5],
+        ["b", 2],
+      ],
+      loop: [
+        ["a", params.loopFrames - 2],
+        ["b", 2],
+      ],
+    });
+  }
 
   draw() {
     b_.setDrawingPattern(Vfx.#checkersPattern);
@@ -30,10 +42,12 @@ export class Vfx {
       v_(b_.canvasSize.x - 8, 2),
       v_(b_.canvasSize.x - 8, b_.canvasSize.y - 8),
     ].forEach((xy) => {
+      const progress =
+        (this.#loopFrames - this.#timer.framesLeft) / this.#loopFrames;
       b_.drawRectFilled(
         xy,
-        v_(6, 6).mul(BpxEasing.outQuadratic(this.#timer.progress)),
-        rgb_p8_.moss,
+        v_(6, 6).mul(BpxEasing.outQuadratic(progress)),
+        this.#timer.currentPhase === "a" ? rgb_p8_.moss : rgb_p8_.orange,
       );
     });
     b_.setDrawingPattern(BpxDrawingPattern.primaryOnly);
