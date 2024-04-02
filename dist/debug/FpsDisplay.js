6e00f87912821af2a2a7b029ef0187d5f947c229
Beetroot Paul <paul@beetrootpaul.com>
2024-04-03 01:35:21 +0200

    improve positioning of FPS display

--- dist/debug/FpsDisplay.js
+++ dist/debug/FpsDisplay.js
@@ -9,21 +9,26 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _FpsDisplay_drawApi, _FpsDisplay_color, _FpsDisplay_xy, _FpsDisplay_recentSamples, _FpsDisplay_nextSampleIndex, _FpsDisplay_lastCalculatedAverageFps;
-import { font_pico8_, rgb_p8_, v_ } from "../shorthands";
+var _FpsDisplay_drawApi, _FpsDisplay_color, _FpsDisplay_canvasSize, _FpsDisplay_alignTop, _FpsDisplay_alignLeft, _FpsDisplay_recentSamples, _FpsDisplay_nextSampleIndex, _FpsDisplay_lastCalculatedAverageFps;
+import { b_ } from "../BeetPx";
+import { font_pico8_, rgb_p8_ } from "../shorthands";
 import { BpxUtils } from "../utils/Utils";
 export class FpsDisplay {
     constructor(drawApi, canvasSize, params) {
         _FpsDisplay_drawApi.set(this, void 0);
         _FpsDisplay_color.set(this, void 0);
-        _FpsDisplay_xy.set(this, void 0);
+        _FpsDisplay_canvasSize.set(this, void 0);
+        _FpsDisplay_alignTop.set(this, void 0);
+        _FpsDisplay_alignLeft.set(this, void 0);
         _FpsDisplay_recentSamples.set(this, void 0);
         _FpsDisplay_nextSampleIndex.set(this, void 0);
         _FpsDisplay_lastCalculatedAverageFps.set(this, void 0);
         __classPrivateFieldSet(this, _FpsDisplay_drawApi, drawApi, "f");
         __classPrivateFieldSet(this, _FpsDisplay_color, params.color ?? rgb_p8_.orange, "f");
+        __classPrivateFieldSet(this, _FpsDisplay_canvasSize, canvasSize, "f");
         const placement = params.placement ?? "top-right";
-        __classPrivateFieldSet(this, _FpsDisplay_xy, v_(placement.endsWith("-left") ? 1 : canvasSize.x - 3 * 4, placement.startsWith("top-") ? 1 : canvasSize.y - 5), "f");
+        __classPrivateFieldSet(this, _FpsDisplay_alignLeft, placement.endsWith("-left"), "f");
+        __classPrivateFieldSet(this, _FpsDisplay_alignTop, placement.startsWith("top-"), "f");
         __classPrivateFieldSet(this, _FpsDisplay_recentSamples, Array.from({ length: 15 }, () => 0), "f");
         __classPrivateFieldSet(this, _FpsDisplay_nextSampleIndex, 0, "f");
         __classPrivateFieldSet(this, _FpsDisplay_lastCalculatedAverageFps, 0, "f");
@@ -40,8 +45,10 @@ export class FpsDisplay {
             __classPrivateFieldSet(this, _FpsDisplay_lastCalculatedAverageFps, __classPrivateFieldGet(this, _FpsDisplay_lastCalculatedAverageFps, "f") / __classPrivateFieldGet(this, _FpsDisplay_recentSamples, "f").length, "f");
         }
         const prevFont = __classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").useFont(font_pico8_);
-        __classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").drawText(__classPrivateFieldGet(this, _FpsDisplay_lastCalculatedAverageFps, "f").toFixed(), __classPrivateFieldGet(this, _FpsDisplay_xy, "f").add(__classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").cameraXy), __classPrivateFieldGet(this, _FpsDisplay_color, "f"));
+        const text = __classPrivateFieldGet(this, _FpsDisplay_lastCalculatedAverageFps, "f").toFixed();
+        const wh = b_.measureText(text).wh;
+        __classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").drawText(text, __classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").cameraXy.add(__classPrivateFieldGet(this, _FpsDisplay_alignLeft, "f") ? 1 : __classPrivateFieldGet(this, _FpsDisplay_canvasSize, "f").x - wh.x - 1, __classPrivateFieldGet(this, _FpsDisplay_alignTop, "f") ? 1 : __classPrivateFieldGet(this, _FpsDisplay_canvasSize, "f").y - wh.y - 1), __classPrivateFieldGet(this, _FpsDisplay_color, "f"));
         __classPrivateFieldGet(this, _FpsDisplay_drawApi, "f").useFont(prevFont);
     }
 }
-_FpsDisplay_drawApi = new WeakMap(), _FpsDisplay_color = new WeakMap(), _FpsDisplay_xy = new WeakMap(), _FpsDisplay_recentSamples = new WeakMap(), _FpsDisplay_nextSampleIndex = new WeakMap(), _FpsDisplay_lastCalculatedAverageFps = new WeakMap();
+_FpsDisplay_drawApi = new WeakMap(), _FpsDisplay_color = new WeakMap(), _FpsDisplay_canvasSize = new WeakMap(), _FpsDisplay_alignTop = new WeakMap(), _FpsDisplay_alignLeft = new WeakMap(), _FpsDisplay_recentSamples = new WeakMap(), _FpsDisplay_nextSampleIndex = new WeakMap(), _FpsDisplay_lastCalculatedAverageFps = new WeakMap();
