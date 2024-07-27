f2d6f646f1b7842223eb909a443fb277808dfd89
Beetroot Paul <paul@beetrootpaul.com>
2024-07-27 21:46:26 +0200

    reuse previous example project as a general playground project for a quick ideas testing

---
+++ playground/src/Vfx.ts
@@ -0,0 +1,62 @@
+import {
+  $,
+  $d,
+  $rgb_p8,
+  $timerSeq,
+  $v,
+  BpxDrawingPattern,
+  BpxEasing,
+  BpxTimerSequence,
+} from "../../src";
+
+export class Vfx {
+  static #checkersPattern = BpxDrawingPattern.from(`
+    ##--
+    ##--
+    --##
+    --##
+  `);
+
+  #loopFrames: number;
+  #timer: BpxTimerSequence<"a" | "b">;
+
+  constructor(params: { loopFrames: number }) {
+    this.#loopFrames = params.loopFrames;
+    this.#timer = $timerSeq(
+      {
+        intro: [
+          ["a", params.loopFrames - 9],
+          ["b", 2],
+        ],
+        loop: [
+          ["a", params.loopFrames - 2],
+          ["b", 2],
+        ],
+      },
+      {
+        onGamePause: "ignore",
+      },
+    );
+  }
+
+  draw() {
+    $d.setDrawingPattern(Vfx.#checkersPattern);
+
+    const rectSize = 16;
+    [
+      $v(2, 2),
+      $v(2, $.canvasSize.y - rectSize - 2),
+      $v($.canvasSize.x - rectSize - 2, 2),
+      $v($.canvasSize.x - rectSize - 2, $.canvasSize.y - rectSize - 2),
+    ].forEach((xy) => {
+      const progress =
+        (this.#loopFrames - this.#timer.framesLeft) / this.#loopFrames;
+      $d.rectFilled(
+        xy,
+        $v(rectSize).mul(BpxEasing.outQuadratic(progress)),
+        this.#timer.currentPhase === "a" ? $rgb_p8.moss : $rgb_p8.orange,
+      );
+    });
+    $d.setDrawingPattern(BpxDrawingPattern.primaryOnly);
+  }
+}
